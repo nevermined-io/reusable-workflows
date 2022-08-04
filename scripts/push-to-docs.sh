3ce35ae6c35c46a7c7c03bb8e5d6a13bb861b086
Javier Cortejoso <javier.cortejoso@gmail.com>
2022-08-04 11:17:03 +0200

    Setting default values in script

--- scripts/push-to-docs.sh
+++ scripts/push-to-docs.sh
@@ -3,14 +3,18 @@
 # Expected env vars for envsubst
 # BRANCH_TO_CLONE=${{ inputs.branch-to-clone }}
 BRANCH_TO_CLONE=${BRANCH_TO_CLONE}
+# Set default value if not set
+BRANCH_TO_CLONE=${BRANCH_TO_CLONE:-master}
 # API_TOKEN_GITHUB=${{ secrets.API_TOKEN_GITHUB }}
 API_TOKEN_GITHUB=${API_TOKEN_GITHUB}
 # REPO_TO_PUSH=${{ inputs.repo-to-push }}
 REPO_TO_PUSH=${REPO_TO_PUSH}
+REPO_TO_PUSH=${REPO_TO_PUSH:-docs}
 # PATH_TO_PUSH=${{ inputs.path-to-push }}
 PATH_TO_PUSH=${PATH_TO_PUSH}
 # PATH_TO_COPY=${{ inputs.path-to-copy }}
-PATH_TO_COPY=${PATH_TO_COPY}
+PATH_TO_COPY=${PATH_TO_COPY}:
+PATH_TO_COPY=${PATH_TO_COPY:-"docs/"}:
 # REPOSITORY_NAME=${{ env.REPOSITORY_NAME }}
 REPOSITORY_NAME=${REPOSITORY_NAME}
 
